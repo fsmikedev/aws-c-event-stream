5171b8661b3c910cfb9f66024bc3948a84e045da
mdevereux <michael.devereux@framestore.com>
2019-12-18 09:59:59 +0000

    fix-build

--- cmake/aws-c-event-stream-config.cmake
+++ cmake/aws-c-event-stream-config.cmake
@@ -2,9 +2,4 @@ include(CMakeFindDependencyMacro)
 find_dependency(aws-c-common)
 find_dependency(aws-checksums)
 
-if (BUILD_SHARED_LIBS)
-    include(${CMAKE_CURRENT_LIST_DIR}/shared/@CMAKE_PROJECT_NAME@-targets.cmake)
-else()
-    include(${CMAKE_CURRENT_LIST_DIR}/static/@CMAKE_PROJECT_NAME@-targets.cmake)
-endif()
-
+include(${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-targets.cmake)
